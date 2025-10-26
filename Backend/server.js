f0112acea4ed35c2b8f3f28bb053fdeb8ac55bed
Priyansh koshti <priyanshkos02@gmail.com>
2025-10-26 14:55:51 +0530

    DB cpnnection is done

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import cors from "cors";
+import mongoose from "mongoose";
 
 
 const app = express();
@@ -11,33 +12,43 @@ app.use(cors());
 
 app.listen(PORT,()=>{
     console.log(`server running on ${PORT}`);
+    connectDB();
 });
 
-app.post("/test",async (req,res)=>{
-    const options = {
-        method:"POST",
-        headers:{
-            "Content-Type":"application/json",
-            "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
-        },
-        body:JSON.stringify({
-            model: "gpt-4o-mini",
-            messages: [
-                     {
-                 role: "user",
-                content: req.body.message
-                        }]
-        })
-    }
-
+const connectDB = async()=>{
     try{
-        const response = await fetch("https://api.openai.com/v1/chat/completions" , options);
-        const data = await response.json();
-        // console.log(data);
-        // res.send(data);
-        console.log(data.choices[0].message.content);
-        res.send(data.choices[0].message.content);
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Connected to DB succesfully");
     }catch(err){
-        console.log(err);
+        console.log("Failed to connect with DB "+err);
     }
-} );
+}
+
+// app.post("/test",async (req,res)=>{
+//     const options = {
+//         method:"POST",
+//         headers:{
+//             "Content-Type":"application/json",
+//             "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+//         },
+//         body:JSON.stringify({
+//             model: "gpt-4o-mini",
+//             messages: [
+//                      {
+//                  role: "user",
+//                 content: req.body.message
+//                         }]
+//         })
+//     }
+
+//     try{
+//         const response = await fetch("https://api.openai.com/v1/chat/completions" , options);
+//         const data = await response.json();
+//         // console.log(data);
+//         // res.send(data);
+//         console.log(data.choices[0].message.content);
+//         res.send(data.choices[0].message.content);
+//     }catch(err){
+//         console.log(err);
+//     }
+// } );
