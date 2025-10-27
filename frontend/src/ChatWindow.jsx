b4bc4219ab816c73e5a007125eec8cc85716ff77
Priyansh koshti <priyanshkos02@gmail.com>
2025-10-27 17:46:42 +0530

    frontend component are prepared

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -1,10 +1,33 @@
 import "./ChatWindow.css";
+import Chat from "./Chat.jsx";
 
 function ChatWindow(){
 
     return(
-       <div>
-        Chatwindow
+       <div className="chatWindow">
+        <div className="navbar">
+            <span>AI-chatbot  <i class="fa-solid fa-arrow-down"></i></span>
+            <div className="userIconDiv">
+               <span className="userIcon">
+                 <i class="fa-solid fa-user"></i>
+               </span>
+            </div>
+        </div>
+        
+        <Chat></Chat>
+
+        <div className="chatInput">
+                <div className="inputBox">
+                    <input placeholder="Ask anything">
+                    </input>
+                    <div id="submit">
+                        <i class="fa-solid fa-paper-plane"></i>
+                    </div>
+                </div>
+                <p className="info">
+                        Chatbot can make mistakes. Cross-Check imp info from your side.
+                        </p>
+        </div>
        </div>
     )
 }
--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -2,9 +2,29 @@ import "./Sidebar.css";
 
 function Sidebar(){
     return (
-        <div>
-            Sidebar
-        </div>
+        <section className="sidebar">
+             {/* new chat button */}
+                <button>
+                    <img src="src/assets/blacklogo.png" alt="gpt logo" className="logo"></img>
+                    <span><i className="fa-solid fa-pen-to-square"></i></span>
+                </button>
+
+
+             {/* history */}
+                <ul className="history">
+                        <li>history1</li>
+                        <li>history 2</li>
+                        <li>history3 </li>
+                </ul>
+
+
+             {/* sign */}
+                <div className="sign">
+                    <p> by priyansh koshti &hearts;</p>
+                </div>
+
+
+        </section>
     )
 }
 
