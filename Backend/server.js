dc8d7f6b7d47ad85d964b7ef88011e49f0b3a2f5
Priyansh koshti <priyanshkos02@gmail.com>
2025-11-04 22:31:30 +0530

    allowed one more frontend link to access backend server

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,9 @@ app.use(express.json());
 // app.use(cors());
 const allowedOrigins = [
   "http://localhost:5173", // local dev
-  "https://your-frontend-domain.com" // replace later after deployment
+  "https://your-frontend-domain.com"  ,// replace later after deployment
+  "http://54.90.94.99:3000" ,
+   "https://aarogyam-4-u.onrender.com",
 ];
 
 app.use(
